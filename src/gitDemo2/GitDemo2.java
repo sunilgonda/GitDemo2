fa1f4f6579a9536b2aa31ba841a58a69c3d02684
sunil gonda <sunilgonda7@gmail.com>
2022-06-24 15:38:32 +0530

    second demo project

---
+++ src/gitDemo2/GitDemo2.java
@@ -0,0 +1,12 @@
+package gitDemo2;
+
+public class GitDemo2 {
+
+	public static void main(String[] args) {
+		System.out.println("demo 2");
+		System.out.println("demo 3");
+
+
+	}
+
+}
---
+++ src/gitDemo2/gitDemo4.java
@@ -0,0 +1,9 @@
+package gitDemo2;
+
+public class gitDemo4 {
+
+	public static void main(String[] args) {
+System.out.println("check");
+	}
+
+}
